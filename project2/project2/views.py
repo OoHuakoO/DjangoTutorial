79b0b58bfff2fc712dd25b5269027300abcc1409
khomsark <komsark_@hotmail.co.th>
2020-10-09 01:18:27 +0700

    commit

--- project2/project2/views.py
+++ project2/project2/views.py
@@ -7,3 +7,12 @@ def show_acticle(request,id,title):
     return HttpResponse(f'ID:{id} <br> Title: {title}')
 def drink(request,dnk):
     return HttpResponse(f'Drink: {dnk}')
+def find(request,key,page):
+    page = int(page)
+    prev = ''
+    if page > 1:
+        prev = f'<a href="/find/{key}/{page-1}">Previous</a>'
+    else:
+        prev = ''
+    next = f'<a href="/find/{key}/{page+1}">Next</a>'
+    return HttpRespones(f'{prev}&nbsp;&nbsp;&nbsp;{next}')
